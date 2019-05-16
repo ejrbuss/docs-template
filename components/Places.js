9aa3a618aa9900c76ce835d0afb20acf5f18f0b5
ejrbuss <ejrbuss@gmail.com>
2019-05-15 20:39:26 -0700

    trying to fix links

--- components/Places.js
+++ components/Places.js
@@ -2,8 +2,8 @@ import Link  from 'next/link';
 
 export default ({ home, api }) => (
     <>
-        <Link href='/index'><a className={`m-md ${home ? 'active' : ''}`}><i className='fas fa-home' />Home</a></Link>
-        <Link href='/api'><a className={`m-md ${api ? 'active' : ''}`}><i className='fas fa-book' />API</a></Link>
-        <Link href='/download'><a className='m-md'><i className='fas fa-cloud-download-alt' />Download</a></Link>
+        <Link href='/docs-template/index'><a className={`m-md ${home ? 'active' : ''}`}><i className='fas fa-home' />Home</a></Link>
+        <Link href='/docs-template/api'><a className={`m-md ${api ? 'active' : ''}`}><i className='fas fa-book' />API</a></Link>
+        <Link href='/docs-template/download'><a className='m-md'><i className='fas fa-cloud-download-alt' />Download</a></Link>
     </>
 );
